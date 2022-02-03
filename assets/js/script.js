2105ebb15a0ed650cc8b3d304151ea0d2cca40ce
Yelena Belova <42819374+kvs1995@users.noreply.github.com>
2022-02-02 23:17:28 -0800

    edit the index.html and add in the function to create the div/p/img tags rather than already being there. add a function to loop through the attributes and add the classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -106,55 +106,134 @@ function initialFetchData() {
         var sunsetEl=document.querySelector('#sunset')
         currentCityEl.textContent = cityName
         currentDateEl.textContent = moment().format("MMMM Do, YYYY")
-        currentTimeEl.textContent = moment().format("hh:mm:ss A")
-        sunriseEl.textContent = "Sunrise: " + moment(weatherData.sys.sunrise, "x").format("h:mm A")
-        sunsetEl.textContent = "Sunset: " + moment(weatherData.sys.sunset, "x").format("h:mm A")
+        currentTimeEl.textContent = moment().format("h:mm:ss a")
+        sunriseEl.textContent = "Sunrise: " + moment(weatherData.sys.sunrise, "X").format("h:mm a")
+        sunsetEl.textContent = "Sunset: " + moment(weatherData.sys.sunset, "X").format("h:mm a")
         
         highEl.textContent = 'High: '+ convertTemp(weatherData.main.temp_max) + '\u00B0 F'
         lowEl.textContent = 'Low: ' + convertTemp(weatherData.main.temp_min) + '\u00B0 F'
-        fetchData(weatherData.coord.lon,  weatherData.coord.lat)
+        var coordinates = [weatherData.coord.lon, weatherData.coord.lat]
+        // fetchData(weatherData.coord.lon,  weatherData.coord.lat)
+        return coordinates
+
+    }) 
+    .then(function(coordinates) {
+        var cityLongitude = coordinates[0]
+        var cityLatitude = coordinates[1]
+        console.log(cityLongitude)
+
+        var apiKey = '410bf7cb489396d2d3451160359de4e0'
+        var oneCallRequestURL='https://api.openweathermap.org/data/2.5/onecall?lat=' + cityLatitude + '&lon=' + cityLongitude + '&appid=' + apiKey
+        fetch(oneCallRequestURL) 
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(oneCallWeatherData) {
+
+                var currentDegreeEl = document.querySelector('#current-degree')
+                var currentWindEl= document.querySelector('#current-wind')
+                var currentHumidityEl = document.querySelector('#current-humidity')
+                var currentUVIndexEl= document.querySelector('#current-uv-index')
+                var weatherIconEl = document.querySelector('#current-icon')
+                var iconURL = 'http://openweathermap.org/img/w/' + oneCallWeatherData.current.weather[0].icon + '.png'
+                console.log(iconURL)
+                weatherIconEl.setAttribute('src',iconURL)
 
 
+                currentDegreeEl.textContent = 'Temp: ' + convertTemp(oneCallWeatherData.current.temp) + '\u00B0 F'
+                currentWindEl.textContent  = 'Wind: ' +  oneCallWeatherData.current.wind_speed +' mph'
+                currentHumidityEl.textContent  = 'Humidity: ' + oneCallWeatherData.current.humidity
+                currentUVIndexEl.textContent  = 'UV Index: ' + oneCallWeatherData.current.uvi
+        
+
+                //////////////////////FORECASTED DAYS//////////////////////
+                var cardsEl=document.querySelector('.cards')
+                
+                function setAttributeHelperFunction(targetEl, attributes){
+                    for (var keys in attributes) {
+                        targetEl.setAttribute(keys, attributes[key])
+                    };
+
+                console.log("hit line 157")
+                //for each element, execute set Attribute for each attribute
+                for (var i=0; i<5; i++) {
+                    var forecastDateEl = document.createElement('h5'); 
+                    var forecastTempEl = document.createElement('div');
+                    var forecastDegreeEl = document.createElement('p');
+                    var forecastIconEl = document.createElement('img');
+                    var forecastWindEl = document.createElement('p');
+                    var forecastHumidityEl = document.createElement('p');
+                    var forecastUVIndexEl = document.createElement('p');
+                    var forecastElList = [forecastDateEl,forecastTempEl,forecastDegreeEl,forecastIconEl,forecastWindEl,forecastHumidityEl,forecastUVIndexEl]
+                    var forecastTypeList = ["date","temp","degree","icon","wind","humidity","uv-index"]
+
+                    console.log("hit line 168")
+                    for (var j=0; j<forecastElList.length; j++) {
+                        var forecastClassName = "forecast-"+forecastTypeList[j]
+                        forecastElList[j].setAttribute("class",forecastClassName)
+                        if (forecastTypeList[j] === "icon"){
+                            var altContent = "Forecast Weather Icon: Day " + (i+1).toString();
+                            forecastElList[j].setAttribute("src",oneCallWeatherData.daily[j].weather[0].icon)
+                            forecastElList[j].setAttribute("alt", altContent)
+                        }
+                    }
+                    console.log("hit line 177")
+                    // "X").format("h:mm a")
+                    // convertTemp(oneCallWeatherData.daily[i].temp)
+                    var forecastWeatherData = {
+                        "date": moment(oneCallWeatherData.daily[i].dt,"X").format("h:mm a"),
+                        "degree": convertTemp(oneCallWeatherData.daily[i].temp), 
+                        "wind": oneCallWeatherData.daily[i].wind_speed,
+                        "humidity": oneCallWeatherData.daily[i].wind_speed,
+                        "uv-index": oneCallWeatherData.daily[i].weather.uvi
+                    }
+                    console.log(forecastWeatherData)
+                    // for (var m = 0; m <  forecastWeatherData.length; m++) {
+                    //     if 
+
+                    // }
+
+                }
+            }
     })
 
-}
+})}
 
-
-function fetchData(longitude, latitude) {
-    var cityLongitude = longitude
-    var cityLatitude = latitude
-    console.log(cityLongitude)
-    var apiKey = '410bf7cb489396d2d3451160359de4e0'
-    var requestURL='https://api.openweathermap.org/data/2.5/onecall?lat=' + cityLatitude + '&lon=' + cityLongitude + '&appid=' + apiKey
-    console.log(requestURL)
-
-    fetch(requestURL) 
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(weatherData) {
-
-        var currentDegreeEl = document.querySelector('#current-degree')
-        var currentWindEl= document.querySelector('#current-wind')
-        var currentHumidityEl = document.querySelector('#current-humidity')
-        var currentUVIndexEl= document.querySelector('#current-uv-index')
-        var weatherIconEl = document.querySelector('#current-icon')
-        var iconURL = 'http://openweathermap.org/img/w/' + weatherData.current.weather[0].icon + '.png'
-        console.log(iconURL)
-        weatherIconEl.setAttribute('src',iconURL)
-
-
-        currentDegreeEl.textContent = 'Temp: ' + convertTemp(weatherData.current.temp) + '\u00B0 F'
-        currentWindEl.textContent  = 'Wind: ' +  weatherData.current.wind_speed +' mph'
-        currentHumidityEl.textContent  = 'Humidity: ' + weatherData.current.humidity
-        currentUVIndexEl.textContent  = 'UV Index: ' +weatherData.current.uvi
- 
-    })
-
-}
 searchButtonEl.addEventListener('click', initialFetchData)
 searchButtonEl.addEventListener('click', addHistory)
+// function fetchData(longitude, latitude) {
+//     var cityLongitude = longitude
+//     var cityLatitude = latitude
+//     console.log(cityLongitude)
+//     var apiKey = '410bf7cb489396d2d3451160359de4e0'
+//     var requestURL='https://api.openweathermap.org/data/2.5/onecall?lat=' + cityLatitude + '&lon=' + cityLongitude + '&appid=' + apiKey
+//     console.log(requestURL)
+
+//     fetch(requestURL) 
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(weatherData) {
+
+//         var currentDegreeEl = document.querySelector('#current-degree')
+//         var currentWindEl= document.querySelector('#current-wind')
+//         var currentHumidityEl = document.querySelector('#current-humidity')
+//         var currentUVIndexEl= document.querySelector('#current-uv-index')
+//         var weatherIconEl = document.querySelector('#current-icon')
+//         var iconURL = 'http://openweathermap.org/img/w/' + weatherData.current.weather[0].icon + '.png'
+//         console.log(iconURL)
+//         weatherIconEl.setAttribute('src',iconURL)
 
 
+//         currentDegreeEl.textContent = 'Temp: ' + convertTemp(weatherData.current.temp) + '\u00B0 F'
+//         currentWindEl.textContent  = 'Wind: ' +  weatherData.current.wind_speed +' mph'
+//         currentHumidityEl.textContent  = 'Humidity: ' + weatherData.current.humidity
+//         currentUVIndexEl.textContent  = 'UV Index: ' +weatherData.current.uvi
+ 
 
+//         //////////////////////FORECASTED DAYS//////////////////////
+//         var forecastCardsEl=document.querySelector('.forecast-cards')
 
+//     })
+
+// 
