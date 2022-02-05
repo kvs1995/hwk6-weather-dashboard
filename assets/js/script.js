e19e652bf5cccfd05f88fc32148b5e05b4bb11d9
Yelena Belova <42819374+kvs1995@users.noreply.github.com>
2022-02-05 03:09:44 -0800

    removed unnecessary .then function that took in the coordinates because found out the previous calls to the APIs persists to the next fetch.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 
 
 var cityInputEl = document.querySelector('.city')
+
 /////////////////// SEARCH HISTORY FUNCTIONALITY /////////////////////
 
 //when I search for a city in the input form field and click the button, the data is fetched from the URl with the filter. 
@@ -20,81 +21,69 @@ function convertTemp(temp) {
 }
 
 //create function to fetch api button when button is click
-function initialFetchData() {
-    // console.log(cityInputEl.value)
+function fetchData(city) {
+    // console.log(city)
+    // cardsEl.innerHTML = ''
 
-    cardsEl.innerHTML = ''
     var cityName = cityInputEl.value
-
     var apiKey = '410bf7cb489396d2d3451160359de4e0'
     var requestURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + apiKey 
 
    fetch(requestURL)
 
-   .then(function(response) {
-       return response.json();
-   })
-
-   .then(function(weatherData) {
-       
-    //////////CREATE ELEMENTS//////////////
-        var currentCityEl = document.querySelector('.current-title')
-        var highEl = document.querySelector('#high')
-        var lowEl = document.querySelector('#low')
-        var currentDateEl = document.querySelector('#current-date')
-        var currentTimeEl = document.querySelector('#current-time')
-        var sunriseEl = document.querySelector('#sunrise')
-        var sunsetEl=document.querySelector('#sunset')
-
-    //////////// SET TEXT CONTENT OF EACH ELEMENT/////////////
-        currentCityEl.textContent = cityName
-        currentDateEl.textContent = moment().format("MMMM Do, YYYY")
-        currentTimeEl.textContent = moment().format("h:mm:ss a")
-        sunriseEl.textContent = "Sunrise: " + moment(weatherData.sys.sunrise, "X").format("h:mm a")
-        sunsetEl.textContent = "Sunset: " + moment(weatherData.sys.sunset, "X").format("h:mm a")
-        highEl.textContent = 'High: '+ convertTemp(weatherData.main.temp_max) + '\u00B0 F'
-        lowEl.textContent = 'Low: ' + convertTemp(weatherData.main.temp_min) + '\u00B0 F'
-        var coordinates = [weatherData.coord.lon, weatherData.coord.lat]
-
-    //return the latitude and longitude to be used in next fetch
-        return coordinates
-
-    }) 
-    .then(function(coordinates) {
-
-        var cityLongitude = coordinates[0]
-        var cityLatitude = coordinates[1]
-
-
-        var apiKey = '410bf7cb489396d2d3451160359de4e0'
-        var oneCallRequestURL='https://api.openweathermap.org/data/2.5/onecall?lat=' + cityLatitude + '&lon=' + cityLongitude +  "&dt=" + moment() +'&appid=' + apiKey
-       
-        fetch(oneCallRequestURL) 
-
         .then(function(response) {
             return response.json();
         })
-        
-        .then(function(oneCallWeatherData) {
 
-        //*********** CREATE ELEMENTS**********//
+        .then(function(weatherData) {
+
+        //////////CREATE ELEMENTS//////////////
+            var currentCityEl = document.querySelector('.current-title')
+            var highEl = document.querySelector('#high')
+            var lowEl = document.querySelector('#low')
+            var currentDateEl = document.querySelector('#current-date')
+            var currentTimeEl = document.querySelector('#current-time')
+            var sunriseEl = document.querySelector('#sunrise')
+            var sunsetEl=document.querySelector('#sunset')
+            var cityLongitude = weatherData.coord.lon
+            var cityLatitude = weatherData.coord.lat
+        //////////// SET TEXT CONTENT OF EACH ELEMENT/////////////
+            currentCityEl.textContent = cityName
+            currentDateEl.textContent = moment().format("MMMM Do, YYYY")
+            currentTimeEl.textContent = moment().format("h:mm:ss a")
+            sunriseEl.textContent = "Sunrise: " + moment(weatherData.sys.sunrise, "X").format("h:mm a")
+            sunsetEl.textContent = "Sunset: " + moment(weatherData.sys.sunset, "X").format("h:mm a")
+            highEl.textContent = 'High: '+ convertTemp(weatherData.main.temp_max) + '\u00B0 F'
+            lowEl.textContent = 'Low: ' + convertTemp(weatherData.main.temp_min) + '\u00B0 F'
+
+            requestURL='https://api.openweathermap.org/data/2.5/onecall?lat=' + cityLatitude + '&lon=' + cityLongitude +  "&dt=" + moment() +'&appid=' + apiKey
+
+            fetch(requestURL) 
+
+            .then(function(response) {
+                return response.json();
+            })
+
+            .then(function(forecastData) {
+
+            //*********** CREATE ELEMENTS**********//
             var currentDegreeEl = document.querySelector('#current-degree')
             var currentWindEl= document.querySelector('#current-wind')
             var currentHumidityEl = document.querySelector('#current-humidity')
             var currentUVIndexEl= document.querySelector('#current-uv-index')
             var weatherIconEl = document.querySelector('#current-icon')
-            var iconURL = 'http://openweathermap.org/img/w/' + oneCallWeatherData.current.weather[0].icon + '.png'
+            var iconURL = 'http://openweathermap.org/img/w/' + forecastData.current.weather[0].icon + '.png'
 
 
-        //*********** SET TEXT CONTENT OF EACH ELEMENT************//
+            //*********** SET TEXT CONTENT OF EACH ELEMENT************//
             weatherIconEl.setAttribute('src',iconURL)
-            currentDegreeEl.textContent = 'Temp: ' + convertTemp(oneCallWeatherData.current.temp) + '\u00B0 F'
-            currentWindEl.textContent  = 'Wind: ' +  oneCallWeatherData.current.wind_speed +' mph'
-            currentHumidityEl.textContent  = 'Humidity: ' + oneCallWeatherData.current.humidity
-            currentUVIndexEl.textContent  = 'UV Index: ' + oneCallWeatherData.current.uvi
-    
+            currentDegreeEl.textContent = 'Temp: ' + convertTemp(forecastData.current.temp) + '\u00B0 F'
+            currentWindEl.textContent  = 'Wind: ' +  forecastData.current.wind_speed +' mph'
+            currentHumidityEl.textContent  = 'Humidity: ' + forecastData.current.humidity
+            currentUVIndexEl.textContent  = 'UV Index: ' + forecastData.current.uvi
 
-    /////////////////////////FORECASTED DAYS////////////////////////////////
+
+            /////////////////////////FORECASTED DAYS////////////////////////////////
 
             // console.log(cardsEl)
             function setAttributeHelperFunction(targetEl, attributes){
@@ -119,34 +108,26 @@ function initialFetchData() {
                     forecastElList[j].setAttribute("class",forecastClassName)
                     if (forecastTypeList[j] === "icon"){
                         var altContent = "Forecast Weather Icon: Day " + (i+1).toString();
-                        var iconURL = 'http://openweathermap.org/img/w/' + oneCallWeatherData.daily[i].weather[0].icon + '.png'
+                        var iconURL = 'http://openweathermap.org/img/w/' + forecastData.daily[i].weather[0].icon + '.png'
                         forecastElList[j].setAttribute("src", iconURL)
                         forecastElList[j].setAttribute("alt", altContent)
                     }
                 }
                 
-                //setting the weather data assignment from the API -- UNNECESSARY RIGHT NOW BUT COULD MAYBE BE USED FOR FOR LOOP
-                var forecastWeatherData = {
-                    "date": moment(oneCallWeatherData.daily[i].dt, "X").format("MMM Do, YYYY"),
-                    "degree": convertTemp(oneCallWeatherData.daily[i].temp.day), 
-                    "wind": oneCallWeatherData.daily[i].wind_speed,
-                    "humidity": oneCallWeatherData.daily[i].humidity,
-                    "uvindex": oneCallWeatherData.daily[i].uvi,
-                }
+                //setting the weather data assignment from the API
 
                 //potentially set to for loop?
-                forecastDateEl.textContent = forecastWeatherData.date
-                forecastDegreeEl.textContent = forecastWeatherData.degree + ' \u00B0F'
-                forecastWindEl.textContent = "Wind: " + forecastWeatherData.wind
-                forecastHumidityEl.textContent ="Humidity: " + forecastWeatherData.humidity
-                forecastUVIndexEl.textContent = "UV Index: " + forecastWeatherData.uvindex
+                forecastDateEl.textContent = moment(forecastData.daily[i].dt, "X").format("MMM Do, YYYY")
+                forecastDegreeEl.textContent = convertTemp(forecastData.daily[i].temp.day) + ' \u00B0F'
+                forecastWindEl.textContent = "Wind: " + forecastData.daily[i].wind_speed
+                forecastHumidityEl.textContent ="Humidity: " + forecastData.daily[i].humidity
+                forecastUVIndexEl.textContent = "UV Index: " + forecastData.daily[i].uvi
                 forecastTempEl.append(forecastDegreeEl, forecastIconEl)
                 cardsEl.append(forecastDateEl,forecastTempEl,forecastWindEl,forecastHumidityEl,forecastUVIndexEl)
 
             }
         })
     })
-
 }
 
 
@@ -164,7 +145,8 @@ function addHistory () {
     //create button to add into search-history
     var historyButton = document.createElement('input');
     historyButton.setAttribute('type','button')
-    historyButton.setAttribute('class', 'history-button')
+    historyButton.setAttribute('class', 'city')
+    historyButton.setAttribute('id', cityName)
     historyButton.setAttribute('value', cityName)
     // historyButton.innerText= cityName
     searchHistoryEl.prepend(historyButton)
@@ -174,7 +156,5 @@ function addHistory () {
 
 
 // module.exports = { addHistory }
-searchButtonEl.addEventListener('click', initialFetchData)
+searchButtonEl.addEventListener('click', fetchData)
 searchButtonEl.addEventListener('click', addHistory)
-
-// module.exports = initialFetchData
